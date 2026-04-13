e4040a7bd64f2f4368f23f0170e5c3d553c28ae2
Alejandro Morales <alejandroleonardos11@gmail.com>
2026-04-13 03:19:17 -0400

    refactor(auth): inyectar variables de entorno asincrona JWT en modulo auth

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,14 +3,20 @@ import { AuthService } from './auth.service';
 import { UsuariosModule } from '../usuarios/usuarios.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsuariosModule,
     PassportModule,
-    JwtModule.register({
-      secret: 'JWT_SECRET',
-      signOptions: { expiresIn: '8h' },
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => {
+        return {
+          secret: ConfigService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: '8h' },
+        };
+      },
     }),
   ],
   providers: [AuthService],
